93ac675a98fea393d7f502438c626f1fe58b2d04
Petr Nymsa <petr.nymsa@netglade.cz>
2024-03-24 11:48:17 +0100

    Add onChange tests

--- glade_forms/test/glade_input_test.dart
+++ glade_forms/test/glade_input_test.dart
@@ -7,4 +7,140 @@ void main() {
 
     expect(input.isValid, isTrue);
   });
+
+  group('onChange tests', () {
+    test('Setter always trigger change', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.value = 100;
+
+      expect(dependentInput.value, equals(100));
+      expect(input.value, equals(100));
+    });
+
+    test('updateValue by default trigger change', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.updateValue(100);
+
+      expect(dependentInput.value, equals(100));
+      expect(input.value, equals(100));
+    });
+
+    test('updateValue disables trigger onChange', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.updateValue(100, shouldTriggerOnChange: false);
+
+      // ignore: avoid-duplicate-test-assertions, it controlls that onChange wasnt called.
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(100));
+    });
+  });
+
+  group('onChange with Controller tests', () {
+    test('Controller always trigger change', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        useTextEditingController: true,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.controller?.text = '100';
+
+      expect(dependentInput.value, equals(100));
+      expect(input.value, equals(100));
+    });
+
+    test('Setter always trigger change', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        useTextEditingController: true,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.value = 100;
+
+      expect(dependentInput.value, equals(100));
+      expect(input.value, equals(100));
+    });
+
+    test('updateValue by default trigger change', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        useTextEditingController: true,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.updateValue(100);
+
+      expect(dependentInput.value, equals(100));
+      expect(input.value, equals(100));
+    });
+
+    test('updateValue disables trigger onChange', () {
+      final dependentInput = GladeInput.intInput(value: -1);
+      final input = GladeInput.intInput(
+        value: 0,
+        useTextEditingController: true,
+        onChange: (info, _) {
+          dependentInput.value = info.value;
+        },
+      );
+
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(0));
+
+      input.updateValue(100, shouldTriggerOnChange: false);
+
+      // ignore: avoid-duplicate-test-assertions, it controlls that onChange wasnt called.
+      expect(dependentInput.value, equals(-1));
+      expect(input.value, equals(100));
+    });
+  });
 }
